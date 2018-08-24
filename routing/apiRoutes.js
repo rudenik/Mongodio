e79a670975ed1ef45e198a265bd676fbeba391ee
Simon Cain <rudenik@gmail.com>
2018-08-24 18:42:03 -0400

    got the card setup and the scrapping working, all new articles are being added to the db.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,15 +11,11 @@ module.exports = function(app) {
         var results = [];
 		request("http://www.gizmodo.com", function(error, response, html) {
 			var $ = cheerio.load(html);
-			// var photo = $('.img-wrapper').each(function(i, element){
-            //     console.log($(element).children().attr('src'));
-            // })
+			
 			$("h1.headline").each(function(i, element) {
-                //shit is broken can't scrape what im looking for. 
-                //try $('article') and then the appropriate childeren from there
-                // $("article").each(function(i, element) {
+                
 				var link = $(element)
-					.children()//.children('.headline')
+					.children()
 					.attr("href");
 				var title = $(element)
                     .children()
@@ -27,48 +23,33 @@ module.exports = function(app) {
 				var photo = $(element)
                     .parent()
                     .siblings('.item__content').children('.asset.main-media').
-                    children('a').children('.img-wrapper').children('picture')//.first().children('img')
-                    .html();
+                    children('a').children('.img-wrapper').children('picture').children('source').first().attr('data-srcset')
+                    
 				var excerpt = $(element)
                     .parent()
                     .siblings('.item__content')
-                    .text();
-                var savedResp;
-                // photo = JSON.stringify(photo);
-                var photoUrl;
-                if(photo){
-                     photoUrl = photo.substring(photo.lastIndexOf('https://'+1), photo.lastIndexOf('png">'));
-                    //  console.log(`photo src: ${photoUrl}png`)
-                     photoUrl = photoUrl.substring(photoUrl.lastIndexOf("https://"), photoUrl.length);
-                     console.log("photoUrl: " + photoUrl);
-                }
-                // photoUrl = photo.substring(photo.lastIndexOf('='+1), photo.lastIndexOf('png">'));
+					.text();
+ 
 				results.push({
 					title: title, 
 					link: link,
 					photo: photo,
 					excerpt: excerpt
                 });
-                // console.log(`photo src: ${photoUrl}`)
-                // console.log(`${link}, photo src: ${JSON.stringify(photo)}`)
-				// db.Article.save({
-				// 	title: title,
-				// 	link: link,
-				// 	photo: photo,
-				// 	excerpt: excerpt
-				// })
-				// 	.then(function(results) {
-				// 		console.log(results);
-				// 		savedResp = results;
-				// 	})
-				// 	.catch(function(err) {
-				// 		if (err) {
-				// 			console.log(err);
-				// 		}
-				// 	});
+                
+				var newArt = new db.Article({
+					title: title,
+					link: link,
+					photo: photo,
+					excerpt: excerpt
+				});
+				newArt.save(function(err, Article){
+					if(err){
+						console.log(err)
+					}
+					console.log(Article);
+				})
 				
-            // });
-            
         });
         
     });
@@ -88,3 +69,4 @@ module.exports = function(app) {
 			});
 	});
 };
+
